384354bbe4a253a4c7af20a309b9b03fe25889a3
vivCoding <vivCoding@gmail.com>
2023-10-26 20:15:12 -0400

    jazz

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme")
+
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
