f6c59dd3301c3de70f359ca235c9bf7c58b1f299
vivCoding <vivCoding@gmail.com>
2023-10-28 22:28:34 -0400

    jazz

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import solid from "solid-start/vite"
 import { defineConfig } from "vite"
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import vercel from "solid-start-vercel"
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [
+    solid({
+      adapter: vercel(),
+    }),
+  ],
   ssr: {
     external: ["@auth/typeorm-adapter"],
   },
